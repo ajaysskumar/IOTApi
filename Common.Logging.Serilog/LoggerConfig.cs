504e5b530715d4f4ee6c12f7b8d17fd256c8d7f3
Ajay kumar <ajay.a338@gmail.com>
2016-12-01 23:55:51 +0530

    removed unused projects

---
+++ Common.Logging.Serilog/LoggerConfig.cs
@@ -0,0 +1,22 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Logging
+{
+    public static class LoggerConfig
+    {
+        public static void InitializeLogger(string connectionString)
+        {
+            var log = new LoggerConfiguration().WriteTo.MSSqlServer(connectionString, "Logs");
+        }
+
+        public static void WriteLog()
+        {
+            
+        }
+    }
+}
